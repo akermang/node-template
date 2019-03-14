1a75bdbde4cbeddbfd54186c7f64a8c85da057f7
Gal Akerman <akermang@gmail.com>
2019-03-15 01:59:34 +0200

    nodemon

--- server/index.js
+++ server/index.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const { readdirSync, statSync } = require('fs')
+const { join } = require('path');
 const http = require('http');
 const bodyParser = require('body-parser');
-
 const internalApi = require('./Api.routes.js');
-
 const app = express();
+
+// const p = path.join('C:','\\');
+// const dirs = p => readdirSync(p);
+// console.log(dirs(p))
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use(express.static(path.join(__dirname, 'src')));
-
-// app.use('/internal', internalApi);
+app.use('/internal', internalApi);
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(`${__dirname}/src/index.html`));
